15aedd4fb1764afc7cdfa6937b80fb4b1b7fe8b4
Bernardo Pacheco <bernardo.pacheco@dominos.com>
2017-12-11 09:51:27 -0500

    Replaced json variable with json-server. Started breaking stuff into components.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-const words = [
-  {
-    text: "kick",
-    imageUrl: "http://weclipart.com/gimg/6D93567D4699DF52/kicking-someone-clipart-1.jpg"
-  },
-  {
-    text: "kitten",
-    imageUrl: "http://www.clker.com/cliparts/F/Z/4/w/c/L/kitten-md.png"
-  },
-  {
-    text: "kite",
-    imageUrl: "https://openclipart.org/download/239399/Colorful-Anthropomorphic-Kite-3.svg"
-  },
-  {
-    text: "key",
-    imageUrl: "http://clipartix.com/wp-content/uploads/2016/08/Key-clip-art-templates-free-clipart-images.jpg"
-  },
-  {
-    text: "kitchen",
-    imageUrl: "http://clipartix.com/wp-content/uploads/2016/06/Free-kitchen-clipart-clip-art-pictures-graphics-illustrations.jpg"
-  }
-]
-
-const Word = ({ text, imageUrl }) => (
-  <div class="word">
-    <img src={imageUrl} />
-    <h1>{text}</h1>
-  </div>
-)
+import NewWord from './components/NewWord'
+import Word from './components/Word'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        {words.map(word => <Word text={word.text} imageUrl={word.imageUrl} />)}
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            words: [],
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/letters/k/words')
+            .then(resp => resp.json())
+            .then(data => {
+                this.setState({
+                    words: data,
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <NewWord />
+                {!this.state.words.length ? (
+                    <div>Loading...</div>
+                ) : (
+                    this.state.words.map(word => (
+                        <Word key={word.id} text={word.text} imageUrl={word.imageUrl} />
+                    ))
+                )}
+            </div>
+        )
+    }
 }
 
 export default App
